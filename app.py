007d5c519f564f3f6a5a1eaa0f40f4fc92d1a507
Lucas Cruz Araújo <lca.snf21@uea.edu.br>
2022-08-17 14:48:27 +0000

    add css

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("Hello")
 
-@app.route("/")
-def hello():
-    return "Hello world"
 
-@app.route("/contatos")
-def contatos():
-    usuario = [1, "O user", "CaraQueUsaSite"]
-    nomeAula = "Aula python para web"
-    return render_template("index.html", nome = nomeAula, usuario = usuario)
+
+posts = [
+    {
+        "title" : "O post vey",
+        "body": "Este post certamente é um post",
+        "author": "cara que escreve posts",
+        "created": datetime(2022,8,17)
+    },
+    {
+        "title" : "Outro post vey",
+        "body": "Este post certamente é outro um post",
+        "author": "cara que escreve posts as vezes",
+        "created": datetime(2022,8,17)
+    },
+    {
+        "title" : "Como ser heroi sem ter poderes.",
+        "body": "Se vista estranho e saia batendo em pessoas na rua",
+        "author": "Batman",
+        "created": datetime(2022,8,17)
+    }
+]
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
