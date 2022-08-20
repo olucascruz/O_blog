3190ffb4ef3ed9b87bbaa5d725fba32b86e4d3d8
LucasCruz <lca.snf21@uea.edu.br>
2022-08-20 02:31:32 -0400

    Add individual post page

--- app.py
+++ app.py
@@ -10,22 +10,34 @@ posts = [
     {
         "title" : wikipedia.page("Mouse").title,
         "body": wikipedia.page("Mouse").summary,
+        "content":wikipedia.page("Mouse").content,
         "author": "cara que escreve posts",
-        "created": datetime(2022,8,17)
+        "created": datetime(2022,8,17),
+        "id": 0
     },
     {
         "title" : "Outro post vey",
         "body": "Este post certamente é outro um post",
         "author": "cara que escreve posts as vezes",
-        "created": datetime(2022,8,17)
+        "created": datetime(2022,8,17),
+        "id": 1
     },
     {
         "title" : "Como ser heroi sem ter poderes.",
         "body": "Se vista estranho e saia batendo em pessoas na rua",
         "author": "Batman",
-        "created": datetime(2022,8,17)
+        "created": datetime(2022,8,17),
+        "id": 2
     }
 ]
 @app.route("/")
 def index():
     return render_template("index.html", posts=posts)
+
+
+@app.route("/posts/<int:id>")
+def post(id):
+    post = posts[id]
+    return render_template("post.html", post=post)
+
+
