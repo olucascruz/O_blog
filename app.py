b2815bd3cde19a054f30ff77554797e079173b74
LucasCruz <lca.snf21@uea.edu.br>
2022-09-07 17:54:33 -0400

    Update Layout

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask("Hello")
 posts = [
     {
         "title" : wikipedia.page("Mouse").title,
-        "body": wikipedia.page("Mouse").summary,
+        "body": "Este post certamente é outro um post",
         "content":wikipedia.page("Mouse").content,
         "author": "cara que escreve posts",
         "created": datetime(2022,8,17),
@@ -18,13 +18,15 @@ posts = [
     {
         "title" : "Outro post vey",
         "body": "Este post certamente é outro um post",
+        "content":wikipedia.page("Instagram").content,
         "author": "cara que escreve posts as vezes",
         "created": datetime(2022,8,17),
         "id": 1
     },
     {
         "title" : "Como ser heroi sem ter poderes.",
-        "body": "Se vista estranho e saia batendo em pessoas na rua",
+        "body": "Não tem como.",
+        "content":wikipedia.page("Batman").content,
         "author": "Batman",
         "created": datetime(2022,8,17),
         "id": 2
