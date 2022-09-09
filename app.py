4907afe7bc56c9f9e024600ff6764d7d6b659e0a
LucasCruz <lca.snf21@uea.edu.br>
2022-09-09 18:02:54 -0400

    Paginação adicionada

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from datetime import datetime
 import wikipedia
+import math
 
 app = Flask("Hello")
 
@@ -30,16 +31,67 @@ posts = [
         "author": "Batman",
         "created": datetime(2022,8,17),
         "id": 2
-    }
+    },
+    {
+        "title" : "Como ser heroi sem ter poderes.",
+        "body": "Não tem como.",
+        "content":wikipedia.page("Batman").content,
+        "author": "Batman",
+        "created": datetime(2022,8,17),
+        "id": 3
+    },
+    {
+        "title" : "Como ser heroi sem ter poderes.",
+        "body": "Não tem como.",
+        "content":wikipedia.page("Batman").content,
+        "author": "Batman",
+        "created": datetime(2022,8,17),
+        "id": 4
+    },
+    {
+        "title" : "Como ser heroi sem ter poderes.",
+        "body": "Não tem como.",
+        "content":wikipedia.page("Batman").content,
+        "author": "Batman",
+        "created": datetime(2022,8,17),
+        "id": 5
+    },
+    {
+        "title" : "Como ser heroi sem ter poderes.",
+        "body": "Não tem como.",
+        "content":wikipedia.page("Batman").content,
+        "author": "Batman",
+        "created": datetime(2022,8,17),
+        "id": 6
+    },
+    {
+        "title" : "Como ser heroi sem ter poderes.",
+        "body": "Não tem como.",
+        "content":wikipedia.page("Batman").content,
+        "author": "Batman",
+        "created": datetime(2022,8,17),
+        "id": 7
+    },
+    {
+        "title" : "Como ser heroi sem ter poderes.",
+        "body": "Não tem como.",
+        "content":wikipedia.page("Batman").content,
+        "author": "Batman",
+        "created": datetime(2022,8,17),
+        "id": 8
+    },
 ]
-@app.route("/")
-def index():
-    return render_template("index.html", posts=posts)
 
+@app.route("/")
+@app.route("/<int:page>")
+def index(page=1):
+    n_post = math.ceil(len(posts)/4)
+    show_posts = [posts[x:x+4] for x in range(0, len(posts), 4)]
+    return render_template("index.html", page=page, posts=show_posts[page-1], n_posts=n_post, )
 
 @app.route("/posts/<int:id>")
 def post(id):
     post = posts[id]
     return render_template("post.html", post=post)
 
-
+app.run(threaded=True)
